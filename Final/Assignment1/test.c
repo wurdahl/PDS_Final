44778226628dbec430388e2999245f37b2429fb8
wurdahl <wurdahl@gmail.com>
2020-04-03 23:21:50 -0500

    updating test.c
    Trying to figure out how ot fake classes in c.

--- Final/Assignment1/test.c
+++ Final/Assignment1/test.c
@@ -1,27 +1,50 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+#include<math.h>
+
+typedef struct shape{
+  double dim;
+  double *computeArea;
+}shape;
+
+double getArea(shape *self){
+  return pow(self->dim,2);
+}
+
+void new_shape(shape *shap){
+  shap->dim=0;
+  shap->computeArea = &getArea;
+}
 
 int main(){
 
-  char *num = "5.3\n";
-  char *input;
+  shape square;
 
-  fgets(input, 100, stdin);
+  new_shape(&square);
 
-  const char *num1 = num;
-  const char *input1 = input;
+  square.dim=2;
 
-  if(strcmp(num1,input1)==0){
-    puts("These are the same string");
-  }else{
-    puts("These aren't the same string");
-    printf("the size of num is %i\n", sizeof(num1));
-    printf("the size of input is %i\n", sizeof(input1));
-  }
+  printf("%lf\n", square->computeArea );
 
-  printf("%f\n", atof(input1));
-  printf("%f\n", strtod(input1, '\0'));
+  // char *num = "5.3\n";
+  // char *input;
+  //
+  // fgets(input, 100, stdin);
+  //
+  // const char *num1 = num;
+  // const char *input1 = input;
+  //
+  // if(strcmp(num1,input1)==0){
+  //   puts("These are the same string");
+  // }else{
+  //   puts("These aren't the same string");
+  //   printf("the size of num is %i\n", sizeof(num1));
+  //   printf("the size of input is %i\n", sizeof(input1));
+  // }
+  //
+  // printf("%f\n", atof(input1));
+  // printf("%f\n", strtod(input1, '\0'));
 
   return 0;
 }
